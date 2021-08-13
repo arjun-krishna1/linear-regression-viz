3c5ea31f2648282b20b24340b8ae724eaadbf71c
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-08-13 16:56:46 -0400

    integrating loss function into base graph code

--- linear_reg_plot.js
+++ linear_reg_plot.js
@@ -1,3 +1,41 @@
+
+//loss function functions
+function makePoint(x, y)
+{
+    return {x: x, y: y};
+}
+
+function makePoints(xs, ys)
+{
+    var points = [];
+    for (var i = 0; i < Math.min(xs.length, ys.length); i++)
+    {
+        points.unshift(makePoint(xs[i], ys[i]));
+    }
+    return points
+}
+
+function stepSize(iloss, sloss, learningRate)
+{
+    return {stepIntercept: iloss * learningRate, stepSlope: sloss * learningRate};
+}
+
+function loss(points, slope, intercept, learningRate = 0.01)
+{
+    var iloss = 0, sloss = 0;
+    points.forEach(point => {
+        iloss += -2 * (point.y - (intercept + slope * point.x));
+        sloss += -2 * point.x * (point.y - (intercept + slope * point.x));
+    });
+    return stepSize(iloss, sloss, learningRate);
+}
+
+function getPoints(slope, inter)
+{
+    return {x: [0, 10], y: [inter, slope * 10 + inter]}
+}
+
+//plotting code
 console.log("plots init")
 
 TESTER = document.getElementById('tester');
@@ -7,6 +45,7 @@ points = {
     "y": [1, 5, 10, 11, 12, 16]
 }
 
+
 let trace_points = {
     x: points.x,
     y: points.y,
@@ -14,20 +53,50 @@ let trace_points = {
     type: 'scatter'
 };
 
-line_points = {
-    "x": [1, 5],
-    "y": [1, 16]
+// line_points = {
+//     "x": [0, 1],
+//     "y": [0, 1]
+// }
+
+// let trace_line = {
+//     "x": line_points.x,
+//     "y": line_points.y,
+//     mode:"line",
+//     type: "scatter"
+// }
+
+function makeData(slope, intercept)
+{
+    let trace_line = {
+        "x": [0, 10],
+        "y": [intercept, slope * 10 + intercept]
+    }
+    return [trace_points, trace_line]
 }
 
-let trace_line = {
-    "x": line_points.x,
-    "y": line_points.y,
-    mode:"line",
-    type: "scatter"
-}
+let slope = -10;
+let intercept = 15; 
 
-let data = [trace_points, trace_line];
+let data = makeData(slope, intercept)
 
-Plotly.newPlot( TESTER, data);
+Plotly.newPlot(TESTER, data);
 
-console.log( Plotly.version );
+// function loop(n)
+// {
+//     console.log(n);
+//     let values = loss(makePoints(points.x, points.y), slope, intercept);
+//     slope -= values.stepSlope;
+//     intercept -= values.stepIntercept;
+//     Plotly.newPlot(TESTER, makeData(slope, intercept));
+//     setTimeout(() => {
+//         loop(n - 1);
+//     }, 1);
+
+// }
+// loop(100);
+
+
+
+
+// console.log( Plotly.version );
+
--- loss.js
+++ loss.js
@@ -3,6 +3,16 @@ function makePoint(x, y)
     return {x: x, y: y};
 }
 
+function makePoints(xs, ys)
+{
+    var points = [];
+    for (var i = 0; i < Math.min(xs.length, ys.length); i++)
+    {
+        points.unshift(makePoint(xs[i], ys[i]));
+    }
+    return points
+}
+
 function stepSize(iloss, sloss, learningRate)
 {
     return {stepIntercept: iloss * learningRate, stepSlope: sloss * learningRate};
@@ -15,11 +25,24 @@ function loss(points, slope, intercept, learningRate = 0.01)
         iloss += -2 * (point.y - (intercept + slope * point.x));
         sloss += -2 * point.x * (point.y - (intercept + slope * point.x));
     });
+    console.log("loss ", iloss, sloss);
     return stepSize(iloss, sloss, learningRate);
 }
 
-
+function getPoints(slope, inter)
+{
+    return {x: [0, 10], y: [inter, slope * 10 + inter]}
+}
 
 let learningRate = 0.01;
 let points = [makePoint(0.5, 1.4), makePoint(2.3, 1.9), makePoint(2.9, 3.2)];
+let slope = 1, inter = 0;
+for (var i =0; i < 10; i++)
+{
+    let values = loss(points, slope, inter);
+
+    slope += values.stepSlope;
+    inter += values.stepIntercept;
+    // console.log("weights ", slope, inter);
+}
 console.log(loss(points, 1, 0));
